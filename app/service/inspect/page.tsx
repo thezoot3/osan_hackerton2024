49bcb1ba9c155c7243f40d008bca4053f57d37d2
thezoot3 <thezoot3@gmail.com>
2024-07-13 17:16:11 +0900

    layout

--- app/service/inspect/page.tsx
+++ app/service/inspect/page.tsx
@@ -2,7 +2,7 @@ import './styles.css'
 import InspectCamera from './InspectCamera'
 export default function Inspect() {
   return (
-    <div className="relative h-dvh w-full shadow-inner shadow-black">
+    <div className="relative h-full w-full shadow-inner shadow-black">
       <InspectCamera />
     </div>
   )
